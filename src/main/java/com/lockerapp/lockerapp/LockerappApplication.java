8d5e5767c93e067db0682abae974518ac5daff56
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-05 11:02:15 -0700

    initial commit

---
+++ src/main/java/com/lockerapp/lockerapp/LockerappApplication.java
@@ -0,0 +1,13 @@
+package com.lockerapp.lockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lockerapp/lockerapp/LockerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockerapp.lockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

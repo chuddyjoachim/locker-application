5657e143079621ffbefaa1b168e6b0e8acd54b34
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-06 07:31:14 -0700

    added Locker entity

---
+++ src/main/java/com/lockerapp/lockerapp/entity/Locker.java
@@ -0,0 +1,28 @@
+package com.lockerapp.lockerapp.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "locker")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Locker {
+    @Id
+    @Column(name = "id", nullable = false)
+    @SequenceGenerator(name = "locker_seq", sequenceName = "locker_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "locker_seq")
+    private long id;
+    private String name;
+    private String city;
+    private String state;
+    private float lng;
+    private float lat;
+}

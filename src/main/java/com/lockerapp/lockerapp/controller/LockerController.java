eacd00ad0fed919a3086c74a5d7e970442eb1fce
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-06 08:47:30 -0700

    updated lockercontroller

--- src/main/java/com/lockerapp/lockerapp/controller/LockerController.java
+++ src/main/java/com/lockerapp/lockerapp/controller/LockerController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/locker")
+@RequestMapping("/api/locker")
 public class LockerController {
 
     @Autowired

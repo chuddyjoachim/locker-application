19ea508a5644a2cd6a88cbe5e4e1b51150c514c4
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-10 02:08:58 -0700

    Added Rental page

---
+++ frontend/src/pages/rental/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Rental:React.FC = () => {
+
+    return (
+        <div className="flex flex-row justify-center items-center w-screen h-screen p-4">
+            <div className="flex flex-row justify-center items-center text-center font-bold text-black">Navigation to the page is successful</div>
+        </div>
+    )
+}
+
+export default Rental

bbd48c6d594d1157b615d9ab89a925d1ddb129e8
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-10 02:05:33 -0700

    updated search component get and send data to glgobal state

--- frontend/src/components/search/index.tsx
+++ frontend/src/components/search/index.tsx
@@ -1,33 +1,73 @@
 import React from 'react'
 import Select from 'react-select'
+import axios from "axios"
+import {useSearchByStore} from "../../lib/stores/useSearchByStore"
+import { serachLocationArray } from '../../lib/constant'
+import { baseUrl } from '../../lib/basUrl'
+import { useInputDataStore } from '../../lib/stores/useInputDataStore'
+import { useFetchData } from '../../lib/stores/useFetchData'
+
 
 const Search:React.FC = () => {
-    const [selected, setSelect] = React.useState({value:"city", label:"city"});
-    React.useEffect(() => {
-        console.log(selected);        
-    }, [selected])
 
-    const handleChange = (selected:any) =>{
-        setSelect(selected)
+    const {searchBy, setSearchBy} = useSearchByStore()
+    const {inputData, setinputData} = useInputDataStore()
+    const {setDataLoading, setFetchData, setDataError} = useFetchData()
+
+    const fetchalldata = async () => {
+        
+        let url_ = `${baseUrl}/getlockerby${searchBy.value}/${inputData}`;     
+
+        setDataLoading(true)
+
+        if(!inputData || (inputData.length <= 0)) {
+            setDataLoading(false)
+            setDataError(false)
+            setDataError(null)
+            setFetchData([])
+            return alert("please Enter city or state")
+         }
+
+         const {data} = await axios.get(url_);
+         
+         if(data && (data.length > 0)){
+             setDataLoading(false)
+             setDataError(false)           
+             setFetchData(data)
+         }
+         if(data.length <= 0){
+             setDataError(true)
+            return setDataLoading(false)        
+         }
+         
     }
-    const selectArray = [
-        {value:"city", label:"city"},
-        {value:"state", label:"state"}
-    ]
+
+    const handleChange = (searchBy:any) =>{
+        setSearchBy(searchBy)
+    }
+    const changeInputData = (inputData:any) => {
+        setinputData(inputData)
+    }
+
+
+    const searchForm = (e: { preventDefault: () => void; }) =>{
+        e.preventDefault(); 
+        fetchalldata()  
+    }
 
     return (
         <div className="max-w-md mt-auto mb-auto top-52 left-10 relative">
-            <div className="wrpper bg-white h-14 flex flex-row items-center flex-grow">
+            <form onSubmit={searchForm} className="wrpper bg-white h-14 flex flex-row items-center flex-grow">
                 <div className="leftbox flex-grow ml-2">
-                <Select options={selectArray} value={selected} onChange={handleChange} />
+                <Select options={serachLocationArray} value={searchBy} onChange={handleChange} />
                 </div>
                 <div className="input_container flex-grow ml-2 h-full">
-                    <input className="flex-grow w-full h-full outline-none" type="text" name="" id="" placeholder="Enter city or state" />
+                    <input className="flex-grow w-full h-full outline-none" value={inputData} onChange={(e)=>{changeInputData(e.target.value)}} type="text" name="" id="" placeholder="Enter city or state" />
                 </div>
                 <div className="submit_button h-full float-right p-1">
-                    <input className="h-full bg-red-500 text-white font-semibold p-3" type="submit" value="Find Locker" />
+                    <input className="h-full bg-red-500 cursor-pointer text-white font-semibold p-3" type="submit" value="Find Locker" />
                 </div>
-            </div>
+            </form>
             
         </div>
     )

5db6d060aded46651128f76932204e18dde8dcb4
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-07-10 02:02:45 -0700

    added props to LockerData component

--- frontend/src/components/lockerdata.tsx
+++ frontend/src/components/lockerdata.tsx
@@ -1,24 +1,33 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
-const LockerData = () => {
-    let size = "Small"
-    let height = 295
-    let width = 460
-    let dept = 520
-    let price = 450
-    let available = 2
+export interface LockerDataProps{
+    locker: {size?: string
+    name?: string
+    price?: number
+    width?: number
+    height?: number
+    depth?: number
+    quantity?: number}
+}
+
+const LockerData:React.FC<LockerDataProps> = ({locker}) => {
+    
     return (
         <div>
             <div className="lockerdata-row ">
                 <div className="wrapper mb-1.5 p-4 py-5 bg-gray-200 flex flex-row font-semibold">
                     <div className="size flex-grow">
-                        {size} H{height}*W{width}*D{dept}mm
+                        {locker.size} H{locker.height}*W{locker.width}*D{locker.depth}mm
                     </div>
-                    <div className="prie flex-grow">N{price} per Item/month</div>
+                    <div className="prie flex-grow">N{locker.price} per Item/month</div>
                     <div className="rentper flex-grow">1N For First Rent</div>
-                    <div className="available flex-grow">{2}{available} Available</div>
+                    <div className="available flex-grow">{locker.quantity} Available</div>
                     <div className="btn flex-grow">
-                        <span className="px-3 py-1 bg-green-400 text-white font-semibold cursor-pointer ">Rent Now</span>
+                        <Link to="/rental" >
+                        <span className="px-3 py-1 bg-green-400 text-white font-semibold cursor-pointer ">Rent Now
+                        </span>
+                        </Link>
                     </div>
                 </div>
 
